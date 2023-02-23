e28a202c6ce4a38a280732a71fa6a4681375abfe
Fernandeess <matheus.rodrigues@sptech.school>
2023-02-23 14:16:11 -0300

    iniciando com o eclipse

---
+++ Java/ExercicosAula02/aprendendo-com-eclipse/src/main/java/com/stefanini/aprendendo/com/eclipse/Aula02.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.stefanini.aprendendo.com.eclipse;
+
+/**
+ *
+ * @author mfrodrigues2
+ */
+public class Aula02 {
+    
+}
