c4499cc93d44084c8aebb8754491a9cbcbe6d88d
Fernandeess <matheus.rodrigues@sptech.school>
2023-02-24 14:12:40 -0300

    primeira aula

---
+++ Java/ExercicosAula02/aprendendo-com-eclipse/src/main/java/com/stefanini/aprendendo/com/eclipse/AulaIntrodutoria.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.stefanini.aprendendo.com.eclipse;
+
+/**
+ *
+ * @author mfrodrigues2
+ */
+public class AulaIntrodutoria {
+    public static void main(String[] args) {
+        System.out.println("Aqui e Raiz");
+    }
+}
